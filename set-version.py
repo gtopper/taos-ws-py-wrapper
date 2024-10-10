21daefb148ab4fb7056aabc8c8edad1471aa7f97
Gal Topper <gal.topper@gmail.com>
2024-10-10 13:16:52 +0800

    Release

--- set-version.py
+++ set-version.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Iguazio
+# Copyright 2020 Iguazio
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,12 +11,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+from os import environ
 
-__version__ = "0.0.0+unstable"
 
-from .tdengine_connection import (  # noqa: F401
-    Field,
-    QueryResult,
-    Statement,
-    TDEngineConnection,
-)
+def set_version():
+    version = environ.get("GITHUB_REF")
+    assert version, "GITHUB_REF is not defined"
+
+    version = version.replace("refs/tags/v", "")
+
+    lines = []
+    init_py = "taoswswrap/__init__.py"
+    with open(init_py) as fp:
+        for line in fp:
+            if "__version__" in line:
+                line = f'__version__ = "{version}"\n'
+            lines.append(line)
+
+    with open(init_py, "w") as out:
+        out.write("".join(lines))
+
+
+set_version()
